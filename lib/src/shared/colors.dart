cf9e9f1b3b71a45915137bd065f1aa90c7800784
alberico <asoares@gdmseeds.com>
2024-08-14 16:04:37 -0300

    feat: add light colors

---
+++ lib/src/shared/colors.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+//Light
+//Brand colors
+const Color lightPrimaryBrandColor = Color(0xffFAE187);
+const Color normalPrimaryBrandColor = Color(0xffF8D247);
+const Color darkPrimaryBrandColor = Color(0xffE0B827);
+
+const Color lightSecondaryBrandColor = Color(0xff6482FF);
+const Color normalSecondaryBrandColor = Color(0xff274FF5);
+const Color darkSecondaryBrandColor = Color(0xff1A38B2);
+
+const Color lightTertiaryBrandColor = Color(0xffFF9D7E);
+const Color normalTertiaryBrandColor = Color(0xffF87147);
+const Color darkTertiaryBrandColor = Color(0xffE75527);
+
+//Base colors
+const Color lightPrimaryBaseColor = Color(0xff555555);
+const Color normalPrimaryBaseColor = Color(0xff333232);
+const Color darkPrimaryBaseColor = Color(0xff000000);
+
+const Color lightSecondaryBaseColor = Color(0xffABABAB);
+const Color normalSecondaryBaseColor = Color(0xff7C7C7C);
+const Color darkSecondaryBaseColor = Color(0xff505050);
+
+const Color lightTertiaryBaseColor = Color(0xffFFFFFF);
+const Color normalTertiaryBaseColor = Color(0xffEDEEF1);
+const Color darkTertiaryBaseColor = Color(0xffDDDDDD);
+
+//System colors
+const Color lightSuccessSystemColor = Color(0xff24DD7D);
+const Color normalSuccessSystemColor = Color(0xff00AF54);
+const Color darkSuccessSystemColor = Color(0xff0F8849);
+
+const Color lightErrorSystemColor = Color(0xffFD5839);
+const Color normalErrorSystemColor = Color(0xffE03616);
+const Color darkErrorSystemColor = Color(0xffBB2D13);
+
+//Material colors
+const Color thickMaterialColor = Color.fromARGB(232, 250, 250, 250);
+const Color regularMaterialColor = Color.fromARGB(200, 250, 250, 250);
+const Color thinMaterialColor = Color.fromARGB(162, 250, 250, 250);
+const Color ultrathinMaterialColor = Color.fromARGB(107, 250, 250, 250);
+
+//Dark
+//todo: add dark colors
+
+
+//Shadow Colors
+//todo: add shadow colors
+
+//no
+const Color kcPrimaryColor = Color(0xff22A45D);
+const Color kcMediumGreyColor = Color(0xff868686);
+const Color kcLightGreyColor = Color(0xffe5e5e5);
+const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
